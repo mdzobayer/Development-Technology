38eab3230ff66671e166ad4736a1eb67e27b743d
mdzobayer <mdzobayer@rocketmail.com>
2019-05-15 11:39:53 +0600

    View Added in C++

---
+++ Model-View-Controller/C++/View/StudentView.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// View
+class StudentView{
+    public:
+        void printStudentDetails(string studentName, string studentRollNo) {
+            cout << "Student: ";
+            cout << "Name: " + studentName << endl;
+            cout << "Roll No: " + studentRollNo << endl;
+        }
+};
