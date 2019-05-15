b4f9506d0126117efd9c423738f36d4e8cbf0744
mdzobayer <mdzobayer@rocketmail.com>
2019-05-15 19:29:39 +0600

    Struct Called from other package

--- Model-View-Controller/Go/main.go
+++ Model-View-Controller/Go/main.go
@@ -1,79 +1,28 @@
+// Reference 1: https://stackoverflow.com/questions/36967419/can-i-create-a-struct-inside-the-package-to-use-outside-in-main-program
+// Reference 2: https://aggarwalarpit.wordpress.com/2017/07/08/creating-your-own-package-in-go/
+
 package main
 
-import "fmt"
-
-//--------------Model----------
-type Student struct {
-	name   string
-	rollNo string
-}
-
-func (s *Student) Student(name, rollNo string) {
-	(*s).name = name
-	(*s).rollNo = rollNo
-}
-func (s *Student) getRollNo() string {
-	return (*s).rollNo
-}
-func (s *Student) getName() string {
-	return (*s).name
-}
-func (s *Student) setName(name string) {
-	(*s).name = name
-}
-func (s *Student) setRollNo(rollNo string) {
-	(*s).rollNo = rollNo
-}
-
-//--------View-----------
-type StudentView struct {
-}
-
-func (s *StudentView) printStudentDetails(studentName, studentRollNo string) {
-	fmt.Println("Student: ")
-	fmt.Println("Name: ", studentName)
-	fmt.Println("Roll No: ", studentRollNo)
-}
-
-//-------------Controller------------
-
-type StudentController struct {
-	model Student
-	view  StudentView
-}
-
-func (s *StudentController) StudentController(model Student, view StudentView) {
-	(*s).model = model
-	(*s).view = view
-}
-
-func (s *StudentController) setStudentName(name string) {
-	(*s).model.setName(name)
-}
-func (s *StudentController) setStudentRollNo(rollNo string) {
-	(*s).model.setRollNo(rollNo)
-}
-func (s *StudentController) getStudentName() string {
-	return (*s).model.getName()
-}
-func (s *StudentController) getStudentRollNo() string {
-	return (*s).model.getRollNo()
-}
-func (s *StudentController) updateView() {
-	(*s).view.printStudentDetails((*s).model.getName(), (*s).model.getRollNo())
-}
+import (
+	"Development-Technology/Model-View-Controller/Go/controller"
+	"Development-Technology/Model-View-Controller/Go/model"
+	"Development-Technology/Model-View-Controller/Go/view"
+)
 
 func main() {
-	var model Student
+	var model model.Student
+
+	//type Student = mdl.Student
 
 	model.Student("Zobayer", "CSE 059 07156")
-	var view StudentView
 
-	var controller StudentController
+	var view view.StudentView
+
+	var controller controller.StudentController
 	controller.StudentController(model, view)
 
-	controller.updateView()
-	controller.setStudentName("Zobayer Mahmud")
-	controller.updateView()
+	controller.UpdateView()
+	controller.SetStudentName("Zobayer Mahmud")
+	controller.UpdateView()
 
 }
